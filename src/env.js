c0f13c20e4a7c112d41e9b68a49993d19fa3ffd9
RickyHsieh <ric113.cs05g@g2.nctu.edu.tw>
2019-07-15 17:39:30 +0800

    Ignore

--- src/env.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyAHQoH0ZAhFrRRKj9alF4m6w3vSOfpbxQ8",
-  authDomain: "crlab-admin.firebaseapp.com",
-  databaseURL: "https://crlab-admin.firebaseio.com",
-  projectId: "crlab-admin",
-  storageBucket: "crlab-admin.appspot.com",
-  messagingSenderId: "452369537329",
-  appId: "1:452369537329:web:bc5f621ac26b863c"
-};
