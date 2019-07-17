b340b40f0936f0402c6ced3405d10e82bfcd6d1d
RickyHsieh <ric113.cs05g@g2.nctu.edu.tw>
2019-07-17 13:46:39 +0800

    Updates

---
+++ precache-manifest.26ce3bcc6dade60db511612632e2d1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969fce56860802115f65371cd2488f46",
+    "url": "/CRLabAdmin/index.html"
+  },
+  {
+    "revision": "bfe6c16daa7d68dcde20",
+    "url": "/CRLabAdmin/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "bfe6c16daa7d68dcde20",
+    "url": "/CRLabAdmin/static/js/2.ed3ac9ae.chunk.js"
+  },
+  {
+    "revision": "d89dde861db594052fde",
+    "url": "/CRLabAdmin/static/js/main.75188f57.chunk.js"
+  },
+  {
+    "revision": "c819d9d9243eca730bed",
+    "url": "/CRLabAdmin/static/js/runtime~main.3085aaee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc2a78def5a7598d85a4b1b415124bc6.js"
+  "/CRLabAdmin/precache-manifest.26ce3bcc6dade60db511612632e2d1f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CRLabAdmin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
