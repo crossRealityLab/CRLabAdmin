e5ac14a9ef9df1404d6a3a3c8d779572658cd2cb
RickyHsieh <ric113.cs05g@g2.nctu.edu.tw>
2019-07-17 11:58:39 +0800

    Updates

---
+++ precache-manifest.dc2a78def5a7598d85a4b1b415124bc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1fb2386877497f43055ace62c1d02b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed5a6fe0cbef8bdfcd7",
+    "url": "/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "7ed5a6fe0cbef8bdfcd7",
+    "url": "/static/js/2.b60b00ce.chunk.js"
+  },
+  {
+    "revision": "379001ee31a5ef13f188",
+    "url": "/static/js/main.219a2dd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
